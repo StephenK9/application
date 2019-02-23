9c6595926365860d6ff101e7ce0b6eefd98afab8
Stephen <stephenk9966@gmail.com>
2019-02-23 17:08:39 +0200

    git status

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="app">
+    {{showing}}
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="sw()">Load Service Worker</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'apper',
+  name: "apper",
+  data:function(){
+    return{
+      showing:'fucker'
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    sw() {
+      if (!'serviceWorker' in navigator){
+        this.showing='No service worker Bitch!'
+      }
+      else{
+        this.showing = 'Service worker works bitch!'
+      }
+   
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -6,3 +6,16 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+if ("serviceWorker" in navigator) {
+  console.log("service worker is in navigator");
+window.addEventListener("load", () => {
+ navigator.serviceWorker
+   .register("swag.js")
+   .then(reg => console.log(`Service worker registered!`))
+   .catch(e => console.log(e));
+});
+} else {
+console.log("No service worker");
+}
